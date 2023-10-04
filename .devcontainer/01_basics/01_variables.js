c83c8eadde247133a01cda15254ac16d0f3eed93
Abhinav Kumar <135168416+Championshi@users.noreply.github.com>
2023-10-04 19:09:39 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "abhi629941@gmail.com"
+var accountpassword = "123456"//we use let generally
+accountCity = "jaipur"
+let accountstate;
+/* preferred not to use var
+because of issue in block scope and functional scope
+*/
+
+//accoutnid = 2 //not allowed 
+accountEmail ="Abhinav12345@gmail.com"
+accountpassword="125867586759"
+accountCity="bihar"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountCity,accountstate])
