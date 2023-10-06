0e125bd2e0d0c14abd370c40d985ab1dcc591626
Abhinav Kumar <135168416+Championshi@users.noreply.github.com>
2023-10-06 20:04:15 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)=
+
+let name="abhinav"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to the power 53 longcane be use in big website like flipkart
+//bigint 
+//string =>" "
+//boolean => standalone value
+//undefined=>means name given but value not assigned
+//symbol =>  for uniqueness
+//object
+console.log(typeof null);
+//null is a object
+//undefined is type
