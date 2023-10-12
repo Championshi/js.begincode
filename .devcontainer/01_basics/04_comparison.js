418e250c4acd5c0e79c9f2561ea7ec092734f7d1
Abhinav Kumar <135168416+Championshi@users.noreply.github.com>
2023-10-12 04:35:46 +0000

    comparison in javascript

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1)
+// console.log(2>= 1)
+// console.log(2<1)
+// console.log(2 ==1)
+// console.log(2!=1)
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+// console.log(null>0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === it check the value strictly
+
+ console.log("2" === 2);  //in this one value is string value other is number so it got me false;
+
